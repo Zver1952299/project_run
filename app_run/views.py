20a7f0b0022bbbb1b42fa910e56b91a3bf8e62dc
Zverkov Evgeny <zver1952299@yandex.ru>
2025-08-05 19:23:01 +0700

    add api/users and filter

--- app_run/views.py
+++ app_run/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+from django.contrib.auth.models import User
 from .models import Run
-from .serializers import RunSerializer
+from .serializers import RunSerializer, UserSerializer
+
 
 @api_view(['GET'])
 def company_details(request):
@@ -14,6 +16,21 @@ def company_details(request):
     }
     return Response(details)
 
+
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all().filter(is_superuser=False)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = self.queryset
+        user_type = self.request.query_params.get('type', None)
+        if user_type == 'athlete':
+            qs = qs.filter(is_staff=False)
+        elif user_type == 'coach':
+            qs = qs.filter(is_staff=True)
+        return qs
