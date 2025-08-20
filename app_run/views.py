eb7fcf5c323ea8e307658db063396d4a99c7e5c3
Zverkov Evgeny <zver1952299@yandex.ru>
2025-08-20 15:54:50 +0700

    bugfix

--- app_run/views.py
+++ app_run/views.py
@@ -250,15 +250,16 @@ class RatingView(APIView):
     def post(self, request, coach_id):
         athlete_id = request.data.get('athlete')
         rating = request.data.get('rating')
-        print(f"DEBUG Coach_id: {coach_id}, athlete_id: {athlete_id}, rating: {rating}")
         athlete = get_object_or_404(User, id=athlete_id)
         coach = get_object_or_404(User, id=coach_id)
-        print(f"DEBUGnAthlete: {athlete}, coach: {coach}")
         list_athlete_ids = coach.subscribers.values_list("athlete_id", flat=True)
-        print(f"DEBUGnList athlete ids: {list_athlete_ids}")
         if int(athlete_id) in list_athlete_ids:
             subscribe = Subscribe.objects.filter(coach_id=coach_id, athlete_id=athlete_id).first()
             if subscribe:
+                if rating is None or not str(rating).isdigit():
+                    return Response({
+                        'detail': 'rating is invalid'
+                    }, status=status.HTTP_400_BAD_REQUEST)
                 subscribe.rating = rating
                 serializer = SubscribeSerializer(subscribe, data={'rating': rating}, partial=True)
                 if serializer.is_valid():
