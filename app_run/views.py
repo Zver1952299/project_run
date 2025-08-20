15bb9281d0ed709cf65fcd0930eac4fdb8c40ef2
Zverkov Evgeny <zver1952299@yandex.ru>
2025-08-20 15:43:20 +0700

    add logs

--- app_run/views.py
+++ app_run/views.py
@@ -250,10 +250,12 @@ class RatingView(APIView):
     def post(self, request, coach_id):
         athlete_id = request.data.get('athlete')
         rating = request.data.get('rating')
-        get_object_or_404(User, id=athlete_id)
+        print(f"Coach_id: {coach_id}, athlete_id: {athlete_id}, rating: {rating}")
+        athlete = get_object_or_404(User, id=athlete_id)
         coach = get_object_or_404(User, id=coach_id)
+        print(f"Athlete: {athlete}, coach: {coach}")
         list_athlete_ids = coach.subscribers.values_list("athlete_id", flat=True)
-
+        print(f"List athlete ids: {list_athlete_ids}")
         if int(athlete_id) in list_athlete_ids:
             subscribe = Subscribe.objects.filter(coach_id=coach_id, athlete_id=athlete_id).first()
             if subscribe:
