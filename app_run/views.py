8548baba68e3c4ab0972719f02ab00c067d107ab
Zverkov Evgeny <zver1952299@yandex.ru>
2025-08-21 13:47:56 +0700

    bugfix avg_speed

--- app_run/views.py
+++ app_run/views.py
@@ -314,6 +314,8 @@ class AnalyticView(APIView):
             .order_by('-avg_speed')
             .first()
         )
+        print(f"DEBUG {speed_avg['avg_speed']}")
+        print(f"DEBUG {type(speed_avg['avg_speed'])}")
 
 
         return Response(
